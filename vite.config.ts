0bf1c67c38bbf5e167da7d3bc8ebfd8658053d59
richqe6E <rikyrivaldi369@gmail.com>
2026-08-02 22:27:40 +0900

    Remove SSR option from tanstackStart configuration

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const config = defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    tanstackStart({ ssr: false }),
+    tanstackStart(),
     viteReact(),
   ],
 })
